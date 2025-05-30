49a5e440042006fd26cf57b6a24b0dbcf4d28559
Frank Marchese <133362490+frankcrafter@users.noreply.github.com>
2025-05-30 13:12:33 +0200

    Update lsp.lua

--- .config/nvim/lua/crafter/after/lsp.lua
+++ .config/nvim/lua/crafter/after/lsp.lua
@@ -49,20 +49,4 @@ require("lspconfig").tailwindcss.setup({
 require("lspconfig").html.setup({
 	capabilities = capabilities,
 })
-
--- Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
-vim.diagnostic.config({
-	underline = true,
-	update_in_insert = false,
-	virtual_text = {
-		spacing = 4,
-		source = "if_many",
-		prefix = "●",
-	},
 })
